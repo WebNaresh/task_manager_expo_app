2731826fad5f75e43ac42aa1c460a4751678abb3
Naresh Bhosale <nareshbhosale73@gmail.com>
2025-02-26 06:42:11 +0530

    Update profile images in ProfileSetup and TaskItem components with new Unsplash URLs

--- app/(admin)/profile/index.tsx
+++ app/(admin)/profile/index.tsx
@@ -142,7 +142,7 @@ export default function ProfileSetup() {
           <View style={styles.imageContainer}>
             <Image
               source={{
-                uri: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-cQfL4nBBRlIcBoOirzokTtE3DcUF3k.png",
+                uri: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
               }}
               style={styles.profileImage}
             />
