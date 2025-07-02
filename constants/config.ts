b56eb18c16ab81c6e5bd39e151a7bf8d75c3ac24
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2025-07-03 00:55:45 +0530

    Update base URL in config; uncomment local URL for reference

--- constants/config.ts
+++ constants/config.ts
@@ -1,2 +1,2 @@
-export const base_url = 'http://192.168.1.4:8000'
-// export const base_url = 'https://taskmanagement-kgnz.onrender.com'
+// export const base_url = 'http://192.168.1.4:8000'
+export const base_url = 'https://taskmanagement-kgnz.onrender.com'
